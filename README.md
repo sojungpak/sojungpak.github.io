8cc872051ad9723eea3d488de720b756aa5fecc8
JY2452 <jaeyoung.pak8@gmail.com>
2021-07-11 00:31:55 -0400

    v1

---
+++ README.md
@@ -0,0 +1,13 @@
+# starterpack-basic
+
+Basic starter package for development. 
+
+## Included in this Starterpack
+* webpack
+* sass
+* babel
+* eslint
+* file-loader
+
+## References
+* [Dartmouth CS 52 Starterpack Tutorial](https://cs52.me/assignments/sa/starterpack/)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('starting up!');
+import './styles/style.scss';
+
+// change require to es6 import style
+import $ from 'jquery';
+
+let sec = 0;
+
+setInterval(() => {
+  const string = `You've been on this page for ${sec} seconds.`;
+  $('#main').html(string);
+  sec += 1;
+  console.log('Hello world');
+}, 1000);
