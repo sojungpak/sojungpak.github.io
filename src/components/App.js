e54e9f83197eda4648838479d6128c457f1e47fb
JY2452 <jaeyoung.pak8@gmail.com>
2023-02-14 21:52:38 -0500

    scroll fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import {
   BrowserRouter as Router,
+  useLocation,
 } from 'react-router-dom';
 
 import Navbar from './navbar';
 import Socials from './socials';
+
+export function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 const App = (props) => {
   const MIN_WIDTH = 800;
@@ -18,6 +29,7 @@ const App = (props) => {
 
   return (
     <Router>
+      <ScrollToTop />
       <Navbar mobile={isMobile} />
       <Socials />
     </Router>
