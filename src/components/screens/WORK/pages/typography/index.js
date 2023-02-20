b8b8dd9f494eaf4f32630ca5892476fbe55669a2
JY2452 <jaeyoung.pak8@gmail.com>
2023-02-20 11:20:09 -0500

    typography done

--- src/components/screens/WORK/pages/typography/index.js
+++ src/components/screens/WORK/pages/typography/index.js
@@ -1,7 +1,9 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState } from 'react';
+import Carousel, { Modal, ModalGateway } from 'react-images';
 import Header from '../header';
 import './style.scss';
+import photos from './photos';
 
 const headerData = {
   name: 'Typography Studies',
@@ -18,10 +20,86 @@ const headerData = {
 };
 
 const Typography = (props) => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = (index) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  };
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
   return (
     <div className={props.mobile ? 'workPageM' : 'workPage'}>
       <Header data={headerData} mobile={props.mobile} />
-      <p>Hello</p>
+
+      <div id="typographyWork">
+        <h2>1 - Poster</h2>
+        <h3>ReelAbilities is the largest film festival for films about people with disabilities</h3>
+        <div className="posterImages">
+          <img id="left" src={photos[0].src} alt="work" onClick={() => openLightbox(0)} />
+          <div id="right">
+            <div className="row">
+              <img src={photos[1].src} alt="work" onClick={() => openLightbox(1)} />
+              <img src={photos[2].src} alt="work" onClick={() => openLightbox(2)} />
+              <img src={photos[3].src} alt="work" onClick={() => openLightbox(3)} />
+            </div>
+            <div className="row" id="row2">
+              <img src={photos[4].src} alt="work" onClick={() => openLightbox(4)} />
+              <img src={photos[5].src} alt="work" onClick={() => openLightbox(5)} />
+              <img src={photos[6].src} alt="work" onClick={() => openLightbox(6)} />
+            </div>
+            <div className="row">
+              <img src={photos[7].src} alt="work" onClick={() => openLightbox(7)} />
+              <img src={photos[8].src} alt="work" onClick={() => openLightbox(8)} />
+              <img src={photos[9].src} alt="work" onClick={() => openLightbox(9)} />
+            </div>
+          </div>
+        </div>
+
+        <h2>2 - Spread</h2>
+        <h3>Introducing the typeface Didot through a 2-page spread</h3>
+        <div className="spreadImages">
+          <img id="left" src={photos[10].src} alt="work" onClick={() => openLightbox(10)} />
+          <div id="right">
+            <div className="row">
+              <img src={photos[11].src} alt="work" onClick={() => openLightbox(11)} />
+              <img src={photos[12].src} alt="work" onClick={() => openLightbox(12)} />
+            </div>
+            <div className="row">
+              <img src={photos[13].src} alt="work" onClick={() => openLightbox(13)} />
+              <img src={photos[14].src} alt="work" onClick={() => openLightbox(14)} />
+            </div>
+            <div className="row">
+              <img src={photos[15].src} alt="work" onClick={() => openLightbox(15)} />
+              <img src={photos[16].src} alt="work" onClick={() => openLightbox(16)} />
+            </div>
+          </div>
+        </div>
+
+        <h2>3 - Animation</h2>
+        <h3>60-second video that highlights the unique characteristics and personality of Didot in the context of its use</h3>
+        <div id="vidPlaceHolder" />
+      </div>
+
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map((x) => ({
+                ...x,
+                src: x.src,
+                caption: x.caption,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
     </div>
   );
 };
---
+++ src/components/screens/WORK/pages/typography/photos.js
@@ -0,0 +1,89 @@
+import image1 from '../../../../../assets/work/typography/typo1.png';
+import image2 from '../../../../../assets/work/typography/typo2.png';
+import image3 from '../../../../../assets/work/typography/typo3.png';
+import image4 from '../../../../../assets/work/typography/typo4.png';
+import image5 from '../../../../../assets/work/typography/typo5.png';
+import image6 from '../../../../../assets/work/typography/typo6.png';
+import image7 from '../../../../../assets/work/typography/typo7.png';
+import image8 from '../../../../../assets/work/typography/typo8.png';
+import image9 from '../../../../../assets/work/typography/typo9.png';
+import image10 from '../../../../../assets/work/typography/typo10.png';
+import image11 from '../../../../../assets/work/typography/typo11.png';
+import image12 from '../../../../../assets/work/typography/typo12.png';
+import image13 from '../../../../../assets/work/typography/typo13.png';
+import image14 from '../../../../../assets/work/typography/typo14.png';
+import image15 from '../../../../../assets/work/typography/typo15.png';
+import image16 from '../../../../../assets/work/typography/typo16.png';
+import image17 from '../../../../../assets/work/typography/typo17.png';
+
+const Photos = [{
+  src: image1,
+  caption: '',
+},
+{
+  src: image2,
+  caption: '',
+},
+{
+  src: image3,
+  caption: '',
+},
+{
+  src: image4,
+  caption: '',
+},
+{
+  src: image5,
+  caption: '',
+},
+{
+  src: image6,
+  caption: '',
+},
+{
+  src: image7,
+  caption: '',
+},
+{
+  src: image8,
+  caption: '',
+},
+{
+  src: image9,
+  caption: '',
+},
+{
+  src: image10,
+  caption: '',
+},
+{
+  src: image11,
+  caption: '',
+},
+{
+  src: image12,
+  caption: '',
+},
+{
+  src: image13,
+  caption: '',
+},
+{
+  src: image14,
+  caption: '',
+},
+{
+  src: image15,
+  caption: '',
+},
+{
+  src: image16,
+  caption: '',
+},
+{
+  src: image17,
+  caption: '',
+},
+];
+
+export default Photos;
